9d39a5124ad6fb6fbdce2a4b1ec1a23ee4c981aa
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-06 21:11:08 +0530

    lab 6 q2

---
+++ numbers.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n, pos=0, neg=0, odd=0, even=0;
+    printf("num of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter %d integers: \n",n);
+    for (int i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+        if (arr[i]>0)
+        {
+            pos++;
+        }
+        else if (arr[i]<0)
+        {
+            neg++;
+        }
+        if (arr[i]%2==0)
+        {
+            even++;
+        }
+        else{
+            odd++;
+        }
+    }
+    printf("positive: %d\n",pos);
+    printf("negativ: %d\n",neg);
+    printf("even: %d\n",even);
+    printf("odd: %d\n",odd);
+    return 0;
+}
