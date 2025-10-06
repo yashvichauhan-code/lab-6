789ea3fda764841a4b75809a2a1105473b022189
Yashvi Chauhan <yashvichauhan@Yashvis-MacBook-Air.local>
2025-10-06 21:18:26 +0530

    lab-6 q3

---
+++ freq.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    int n, num, freq=0, i;
+    printf("enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+    printf("enter %d intergers: \n", n);
+    for (i=0; i<n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    printf("enter num whose frequency is to be found: ");
+    scanf("%d", &num);
+    for (i=0; i<n; i++)
+    {
+        if (arr[i]==num)
+        {
+            freq++;
+        }
+    }
+    printf("frequency of %d is %d\n", num, freq);
+    return 0;
+}
