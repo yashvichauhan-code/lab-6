7435fd45253075e3069f1f5ecf41efa7cbb4eda3
Yashvi Chauhan <yashvi.chauhan1909@gmail.com>
2025-10-27 15:28:56 +0530

    lab6

---
+++ matrix.c
@@ -0,0 +1,9 @@
+//WAP that reads two matrices A (m x n) and B (p x q) and computes the product A and B. 
+#include<stdio.h>
+int main()
+{
+    int arr1[20][20], arr2[20][20], res[20][20], m,n,p,q;
+    printf("for first matrix\n");
+    printf("enter number of rows: ");
+
+}
---
+++ secondlargest.c
@@ -0,0 +1,38 @@
+//Write a C program to print the second largest integer in a list of integers.
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("enter number of integers you want to input: ");
+    scanf("%d", &num);
+    int n[num];
+    printf("enter %d intergers\n",num);
+    for (int i=0; i<=num; i++){
+        scanf("%d", &n[i]);
+    }
+    int large, large2;
+    if (n[0] > n[1]) {
+        large = n[0];
+        large2 = n[1];
+    } else {
+        large = n[1];
+        large2 = n[0];
+    }
+    for (int i=2; i<num; i++){
+        if (n[i]>large){
+            large2=large;
+            large=n[i];
+        }
+        else if (n[i]>large2 && n[i]!=large){
+            large2=n[i];
+        }
+    }
+    printf("the largest num is %d\n", large);
+    if (large==large2){
+        printf("no largest");
+    }
+    else {
+        printf("the second largest number is %d\n", large2);
+    }
+return 0;
+}
